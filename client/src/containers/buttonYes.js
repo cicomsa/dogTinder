f4fe280b2bd7f0c040b1293199d97489f1952ef3
Gersom <kgveld@gmail.com>
2018-04-12 11:26:08 +0200

    inserted fetchAPI

--- client/src/containers/buttonYes.js
+++ client/src/containers/buttonYes.js
@@ -2,22 +2,30 @@ import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import './buttonYes.css'
+import { randomImage } from '../actions/randomImage'
 
-export default class ButtonYes extends PureComponent {
 
-  clickHandler = () => {
-	   console.log("Clicked")
+
+class ButtonYes extends PureComponent {
+   
+like() {
+	this.props.dispatch(randomImage())    
+    
     }
 
     render() {
 	     return(
 	        <div>
-	         <button className="buttonYes" onClick ={this.clickHandler}><i class="fas fa-heart"></i></button>
+	             <button className="buttonYes" onClick ={() => this.like()}><i class="fas fa-heart"></i></button>
           </div>
 	       )
     }
 }
 
+const mapStateToProps = function (state) {
 
-
-//export default connect(mapStateToProps)(ButtonYes)
+    return {
+	showImage: state.randomImage.message
+    }
+}
+export default connect(mapStateToProps)(ButtonYes)
