c8a0ca5193388be2a48a77faa30893653eda3134
Emacdyz <emeline.capdupuy@gmail.com>
2018-04-12 11:33:33 +0200

    api is in index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const app = express()
-const usersRouter = require('./users/router')
-const bodyParser = require('body-parser')
+// const usersRouter = require('./users/router')
+const Sequelize = require('sequelize')
+// const sequelize = require('../db')
+// const bodyParser = require('body-parser')
+const sequelize = new Sequelize('postgres://postgres:secret@localhost:5432/postgres')
 
 app.listen(4001, () => console.log('Express API listening on port 4001'))
 
@@ -11,3 +14,108 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE')
   next()
 })
+
+const Users = sequelize.define('users', {
+  id: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    primaryKey: true
+  },
+  name: {
+    type: Sequelize.STRING,
+
+  },
+  preferences: {
+    type: Sequelize.INTEGER,
+
+  }},
+  {tableName: 'users',
+  timestamps: false}
+)
+
+app.get('/users', (req, res) => {
+  	Users.findAll({
+  	  attributes: ['id', 'name', 'preferences']
+  	})
+  	  .then(result => {
+  	    res.send({
+  	    	users: result
+  	    })
+  	  })
+  	  .catch(err => {
+  	    res.status(500).send({error: 'Something went wrong with Postgres'})
+  	  })
+})
+
+app.get('/users/:id', (req, res) => {
+    const usersId = req.params.id
+    Users.findById(usersId)
+    	  .then(result => {
+    	  	if (!result) {
+    	  		res.status(404).send({error: 'Does not exist'})
+    	  	}
+    	  	else {
+    	  		res.send(result)
+    	  	}
+    	  })
+    	  .catch(err => {
+    	    res.status(500).send({error: 'Something went wrong with Postgres'})
+    	  })
+})
+
+// app.post('/users', (req, res) => {
+//     const users = req.body
+//       console.log(users)
+//
+//     Users.create(users).then(entity => {
+//
+//       // send back the 201 Created status and the entity
+//       res.status(201).send(entity)
+//     })
+// })
+
+// app.put('/users/:id', (req, res) => {
+//     const userId = Number(req.params.id)
+//     const updates = req.body
+//
+//       // find the user in the DB
+//     Users.findById(req.params.id)
+//         .then(entity => {
+//           // change the user and store in DB
+//           return entity.update(updates)
+//         })
+//         .then(final => {
+//           // respond with the changed product and status code 200 OK
+//           res.send(final)
+//         })
+//         .catch(error => {
+//           res.status(500).send({
+//             message: `Something went wrong`,
+//             error
+//           })
+//         })
+// })
+
+// app.delete('/users/:id', (req, res) => {
+//     const usersId = Number(req.params.id)
+//
+//     Users.findById(req.params.id)
+//     	  .then(entity => {
+//     	    // change the users and store in DB
+//     	    return entity.destroy()
+//     	  })
+//     	  .then(_ => {
+//     	    // respond with the changed product and status code 200 OK
+//     	    res.send({
+//     	      message: 'The product was deleted succesfully'
+//     	    })
+//     	  })
+//     	  .catch(error => {
+//     	    res.status(500).send({
+//     	      message: `Something went wrong`,
+//     	      error
+//     	    })
+//     	  })
+// })
+
+// module.exports = users
--- server/users/model.js
+++ server/users/model.js
@@ -1,22 +1,22 @@
-const Sequelize = require('sequelize')
-const sequelize = require('../db')
-
-const Users = sequelize.define('users', {
-  id: {
-    type: Sequelize.INTEGER,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: {
-    type: Sequelize.STRING,
-
-  },
-  preferences: {
-    type: Sequelize.INTEGER,
-
-  }},
-  {tableName: 'users',
-  timestamps: false}
-)
-
-module.exports = Users
+// const Sequelize = require('sequelize')
+// const sequelize = require('../db')
+//
+// const Users = sequelize.define('users', {
+//   id: {
+//     type: Sequelize.INTEGER,
+//     allowNull: false,
+//     primaryKey: true
+//   },
+//   name: {
+//     type: Sequelize.STRING,
+//
+//   },
+//   preferences: {
+//     type: Sequelize.INTEGER,
+//
+//   }},
+//   {tableName: 'users',
+//   timestamps: false}
+// )
+//
+// module.exports = Users
