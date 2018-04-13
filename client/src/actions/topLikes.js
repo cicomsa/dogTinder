f08d5157000528245516be9452f9c9ab0c307373
cicomsa <ioanacomsa85@gmail.com>
2018-04-13 16:03:20 +0200

    complete

---
+++ client/src/actions/topLikes.js
@@ -0,0 +1,14 @@
+import * as request from 'superagent'
+import {FETCHED_DETAILED_USER} from './index'
+
+const baseUrl = 'http://localhost:4001'
+
+export const topLikes = (userId) => (dispatch) => {
+  request
+    .get(`${baseUrl}/users/${userId}`)
+    .then(response => dispatch({
+      type: FETCHED_DETAILED_USER,
+      payload: response.body
+    }))
+    .catch(err => console.log(err))
+}
---
+++ client/src/containers/users.js
@@ -0,0 +1,23 @@
+const usersArray = [
+  {name: 'wouter', likes: 1},
+  {name: 'henk', likes: 5},
+  {name: 'mimi', likes: 6},
+  {name: 'arno', likes: 8},
+  {name: 'jan', likes: 2},
+  {name: 'piet', likes: 3},
+  {name: 'abbas', likes: 9},
+  {name: 'ioana', likes: 10},
+  {name: 'gersom', likes: 4},
+  {name: 'emeline', likes: 7},
+  {name: 'david', likes: 9}
+
+]
+
+const array1 = usersArray.map(a => a.name);
+const array2 = usersArray.map(a => a.likes);
+
+
+var newArray = array1.map((e, i) => e + `: ${array2[i]}`);
+console.log(newArray);
+
+console.log(newArray)
